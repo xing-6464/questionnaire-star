726da223fa4b5f15bbb9d86cd4d24672f4d12669
xing-6464 <xing_6464@foxmail.com>
2023-08-19 22:50:26 +0800

    chore: created 3 layout pages

---
+++ src/layouts/ManageLayout.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import styles from './ManageLayout.module.scss'
+
+const ManageLayout: FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p>ManageLayout left</p>
+        <button>创建问卷</button>
+        <br />
+        <button>我的问卷</button>
+        <br />
+        <button>星标问卷</button>
+        <br />
+        <button>回收站</button>
+      </div>
+      <div className={styles.right}>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default ManageLayout
--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react'
 import QuestionCard from '../../components/QuestionCard'
-import styles from '../manage/List.module.scss'
+import styles from './List.module.scss'
 
 const rawQuestionList = [
   {
