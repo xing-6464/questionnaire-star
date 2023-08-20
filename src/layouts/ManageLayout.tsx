4e9513cbb7009a7282250808061358d08545fdb7
xing-6464 <xing_6464@foxmail.com>
2023-08-20 23:23:03 +0800

    refactor: antd and ManageLayout component

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,19 +1,46 @@
 import React, { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
+import { Button, Space, Divider } from 'antd'
 import styles from './ManageLayout.module.scss'
 
 const ManageLayout: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>ManageLayout left</p>
-        <button>创建问卷</button>
-        <br />
-        <button>我的问卷</button>
-        <br />
-        <button>星标问卷</button>
-        <br />
-        <button>回收站</button>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            创建问卷
+          </Button>
+          <Divider style={{ borderTop: 'transparent' }} />
+          <Button
+            type={pathname.startsWith('/manage/list') ? 'default' : 'text'}
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => nav('/manage/list')}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/star') ? 'default' : 'text'}
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => nav('/manage/star')}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/trash') ? 'default' : 'text'}
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => nav('/manage/trash')}
+          >
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
