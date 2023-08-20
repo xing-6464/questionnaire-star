4926e063c7f57f4eb3990c24814999b2ad9bce06
xing-6464 <xing_6464@foxmail.com>
2023-08-20 22:53:11 +0800

    refactor: antd and MainLayout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,23 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+
+import styles from './MainLayout.module.scss'
+
+const { Header, Content, Footer } = Layout
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>小星问卷 &copy; 2023 - present</Footer>
+    </Layout>
   )
 }
 
