a25f01b056c4e6071bdf7c667e256654fd6a11ee
xing-6464 <xing_6464@foxmail.com>
2023-08-23 00:38:29 +0800

    chore: add Logo component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
 
 import styles from './MainLayout.module.scss'
+import Logo from '../components/Logo'
 
 const { Header, Content, Footer } = Layout
 
@@ -10,7 +11,9 @@ const MainLayout: FC = () => {
   return (
     <Layout>
       <Header className={styles.header}>
-        <div className={styles.left}>logo</div>
+        <div className={styles.left}>
+          <Logo />
+        </div>
         <div className={styles.right}>登录</div>
       </Header>
       <Content className={styles.main}>
