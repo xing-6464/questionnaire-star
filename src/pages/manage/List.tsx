e27de57cfad948e8eaa19f7871c0e9fb8c99b849
xing-6464 <xing_6464@foxmail.com>
2023-08-27 17:05:35 +0800

    refactor: questionCard component

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -2,7 +2,7 @@ import React, { FC, useState } from 'react'
 // import { useSearchParams } from 'react-router-dom'
 import { useTitle } from 'ahooks'
 import QuestionCard from '../../components/QuestionCard'
-import styles from './List.module.scss'
+import styles from './common.module.scss'
 
 const rawQuestionList = [
   {
@@ -46,13 +46,14 @@ const List: FC = () => {
         <div className={styles.right}>搜索</div>
       </div>
       <div className={styles.content}>
-        {questionList.map(q => {
-          const { _id } = q
+        {questionList.length > 0 &&
+          questionList.map(q => {
+            const { _id } = q
 
-          return <QuestionCard key={_id} {...q} />
-        })}
+            return <QuestionCard key={_id} {...q} />
+          })}
       </div>
-      <div className={styles.footer}>footer</div>
+      <div className={styles.footer}>上划加载更多...</div>
     </>
   )
 }
