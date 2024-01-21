fd0cf04023b58f3526e5ca240a2d6f09c15b399b
unknown <17621404964@163.com>
2024-01-21 17:55:51 +0800

    chore: 使用AntD的Form组件开发注册页

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,53 @@
 import React, { FC } from 'react'
+import { Typography, Space, Form, Input, Button } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
+import { LOGIN_PATHNAME } from '../router'
+
+const { Title } = Typography
 
 const Register: FC = () => {
-  return <p>Register</p>
+  function onFinish(value: any) {
+    console.log(value)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>新用户注册</Title>
+        </Space>
+      </div>
+      <div>
+        <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} onFinish={onFinish}>
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="确认密码" name="confirm">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="昵称" name="nickname">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to={LOGIN_PATHNAME}>已有账户，登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
