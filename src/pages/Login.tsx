f4573c4043a3d1aa55d9e134c78b14554dc7e58e
unknown <17621404964@163.com>
2024-01-22 10:25:27 +0800

    chore: 使用AntD的Form组件开发登录页，并实现记住我功能

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,89 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
+import { REGISTER_PATHNAME } from '../router'
+
+const { Title } = Typography
+const USERNAME = 'USERNAME'
+const PASSWORD = 'PASSWORD'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME, username)
+  localStorage.setItem(PASSWORD, password)
+}
+
+function deleteUserFromStorage() {
+  localStorage.removeItem(USERNAME)
+  localStorage.removeItem(PASSWORD)
+}
+
+function getUserInfoFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME),
+    password: localStorage.getItem(PASSWORD),
+  }
+}
 
 const Login: FC = () => {
-  return <p>Login</p>
+  const [form] = Form.useForm()
+
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+
+  function onFinish(values: any) {
+    const { username, password, remember } = values
+
+    console.log(values)
+    if (remember) {
+      rememberUser(username, password)
+    } else {
+      deleteUserFromStorage()
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          form={form}
+        >
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
