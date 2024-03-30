d53e27b7c0f17bf459106c2880068e2bab990f03
xing-6464 <17621404964@163.com>
2024-03-30 11:06:13 +0800

    chore: 使用useRequest获取问卷列表

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,45 +1,18 @@
-import React, { FC, useState } from 'react'
-// import { useSearchParams } from 'react-router-dom'
-import { useTitle } from 'ahooks'
-import { Typography } from 'antd'
+import React, { FC } from 'react'
+import { useRequest, useTitle } from 'ahooks'
+import { Spin, Typography } from 'antd'
 import QuestionCard from '../../components/QuestionCard'
 import ListSearch from '../../components/ListSearch'
 import styles from './common.module.scss'
+import { getQuestionListService } from '../../services/question'
 
 const { Title } = Typography
 
-const rawQuestionList = [
-  {
-    _id: 'q1',
-    title: '问卷1',
-    isPublished: false,
-    isStar: false,
-    answerCount: 5,
-    createdAt: '3月10日 13:23',
-  },
-  {
-    _id: 'q2',
-    title: '问卷2',
-    isPublished: true,
-    isStar: true,
-    answerCount: 5,
-    createdAt: '3月11日 13:23',
-  },
-  {
-    _id: 'q3',
-    title: '问卷3',
-    isPublished: false,
-    isStar: true,
-    answerCount: 6,
-    createdAt: '3月12日 13:23',
-  },
-]
-
 const List: FC = () => {
   useTitle('小星问卷 - 我的问卷')
-  // const [searchParams] = useSearchParams()
-  // console.log('keyword', searchParams.get('keyword'))
-  const [questionList, setQuestionList] = useState(rawQuestionList)
+
+  const { data = {}, loading } = useRequest(getQuestionListService)
+  const { list = [], total = 0 } = data
 
   return (
     <>
@@ -52,8 +25,14 @@ const List: FC = () => {
         </div>
       </div>
       <div className={styles.content}>
-        {questionList.length > 0 &&
-          questionList.map(q => {
+        {loading && (
+          <div style={{ textAlign: 'center' }}>
+            <Spin />
+          </div>
+        )}
+        {!loading &&
+          list.length > 0 &&
+          list.map((q: any) => {
             const { _id } = q
 
             return <QuestionCard key={_id} {...q} />
