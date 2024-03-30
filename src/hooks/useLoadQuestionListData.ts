bce28bf7dea8ad62353e8464a77a0670c5232dd8
xing-6464 <17621404964@163.com>
2024-03-30 12:23:31 +0800

    chore: 开发分页功能-定义URL参数page和pageSize

--- src/hooks/useLoadQuestionListData.ts
+++ src/hooks/useLoadQuestionListData.ts
@@ -1,6 +1,11 @@
 import { useRequest } from 'ahooks'
 import { useSearchParams } from 'react-router-dom'
-import { LIST_SEARCH_PARAM_KEY } from '../constant'
+import {
+  LIST_PAGE_PARAM_KEY,
+  LIST_PAGE_SIZE,
+  LIST_PAGE_SIZE_PARAM_KEY,
+  LIST_SEARCH_PARAM_KEY,
+} from '../constant'
 import { getQuestionListService } from '../services/question'
 
 type OptionType = {
@@ -15,8 +20,10 @@ function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
   const { data, loading, error } = useRequest(
     async () => {
       const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+      const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || '') || 1
+      const pageSize = parseInt(searchParams.get(LIST_PAGE_SIZE_PARAM_KEY) || '') || LIST_PAGE_SIZE
 
-      const data = await getQuestionListService({ keyword, isStar, isDeleted })
+      const data = await getQuestionListService({ keyword, isStar, isDeleted, page, pageSize })
       return data
     },
     {
--- src/services/question.ts
+++ src/services/question.ts
@@ -4,6 +4,8 @@ type SearchOption = {
   keyword: string
   isStar: boolean
   isDeleted: boolean
+  page: number
+  pageSize: number
 }
 
 // 获取单个问卷信息
