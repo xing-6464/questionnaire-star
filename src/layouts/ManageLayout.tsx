a8db6bfa07fd2a9560478d6fabbf926b3a19ad54
xing-6464 <17621404964@163.com>
2024-03-30 08:04:08 +0800

    chore:  开发新建问卷功能-设置loading状态以优化体验

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,18 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
-import { Button, Space, Divider } from 'antd'
+import { Button, Space, Divider, message } from 'antd'
 import styles from './ManageLayout.module.scss'
+import { createQuestionService } from '../services/question'
 
 const ManageLayout: FC = () => {
   const nav = useNavigate()
   const { pathname } = useLocation()
 
+  const [loading, setLoading] = useState(false)
+  async function handleCreateClick() {
+    setLoading(true)
+    const data = await createQuestionService()
+    const id = data || {}
+    if (id) {
+      nav(`/question/edit/${id}`)
+      message.success('创建成功')
+    }
+
+    setLoading(false)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <Space direction="vertical">
-          <Button type="primary" size="large" icon={<PlusOutlined />}>
+          <Button
+            type="primary"
+            size="large"
+            icon={<PlusOutlined />}
+            onClick={handleCreateClick}
+            loading={loading}
+          >
             创建问卷
           </Button>
           <Divider style={{ borderTop: 'transparent' }} />
