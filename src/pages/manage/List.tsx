f46bf8d9d330d161541d50f6aa219835faeab1ab
xing-6464 <17621404964@163.com>
2024-05-06 23:53:23 +0800

    chore: loadMore上划加载更多-真正加载数据

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,23 +1,47 @@
 import React, { FC, useEffect, useRef, useState } from 'react'
-import { useTitle, useDebounceFn } from 'ahooks'
+import { useTitle, useDebounceFn, useRequest } from 'ahooks'
 import { Spin, Typography } from 'antd'
 import QuestionCard from '../../components/QuestionCard'
 import ListSearch from '../../components/ListSearch'
 import styles from './common.module.scss'
 import { useSearchParams } from 'react-router-dom'
+import { getQuestionListService } from '../../services/question'
+import { LIST_PAGE_SIZE, LIST_SEARCH_PARAM_KEY } from '../../constant'
 
 const { Title } = Typography
 
 const List: FC = () => {
   useTitle('小星问卷 - 我的问卷')
 
+  const [searchParams] = useSearchParams()
   const containerRef = useRef<HTMLDivElement>(null)
   const [page, setPage] = useState(1)
   const [list, setList] = useState([])
   const [total, setTotal] = useState(0)
+
   const haveMoreData = total > list.length
 
-  const [searchParams] = useSearchParams()
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListService({
+        page,
+        pageSize: LIST_PAGE_SIZE,
+        keyword: searchParams.get(LIST_SEARCH_PARAM_KEY) || '',
+      })
+
+      return data
+    },
+    {
+      manual: true,
+      onSuccess(res) {
+        const { list: l = [], total = 0 } = res
+        setList(list.concat(l))
+        setTotal(total)
+        setPage(page + 1)
+      },
+    }
+  )
+
   const { run: tryLoadMore } = useDebounceFn(
     () => {
       const ele = containerRef.current
@@ -27,7 +51,7 @@ const List: FC = () => {
       if (domRect == null) return
       const { bottom } = domRect
       if (bottom <= document.body.clientHeight) {
-        console.log('执行加载')
+        load()
       }
     },
     {
@@ -41,14 +65,14 @@ const List: FC = () => {
 
   // 页面滚动时，试加载
   useEffect(() => {
-    // if (haveMoreData) {
-    window.addEventListener('scroll', tryLoadMore)
-    // }
+    if (haveMoreData) {
+      window.addEventListener('scroll', tryLoadMore)
+    }
 
     return () => {
       window.removeEventListener('scroll', tryLoadMore)
     }
-  }, [searchParams])
+  }, [searchParams, haveMoreData])
 
   return (
     <>
@@ -60,8 +84,8 @@ const List: FC = () => {
           <ListSearch />
         </div>
       </div>
-      <div className={styles.content} style={{ height: '2000px' }}>
-        {/* {loading && (
+      <div className={styles.content}>
+        {loading && (
           <div style={{ textAlign: 'center' }}>
             <Spin />
           </div>
@@ -72,7 +96,7 @@ const List: FC = () => {
             const { _id } = q
 
             return <QuestionCard key={_id} {...q} />
-          })} */}
+          })}
       </div>
       <div className={styles.footer}>
         <div ref={containerRef}>loadMore... 上滑加载更多</div>
