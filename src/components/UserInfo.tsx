8b9eca2b243540abaefc0179cf99047ee30c8ac1
xing-6464 <17621404964@163.com>
2024-05-11 23:04:12 +0800

    chore: 开发获取用户信息，以及退出登录

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,15 +1,42 @@
 import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { LOGIN_PATHNAME } from '../router'
+import { useRequest } from 'ahooks'
+import { getUserInfoService } from '../services/user'
+import { UserOutlined } from '@ant-design/icons'
+import { Button, message } from 'antd'
+import { removeToken } from '../utils/user-token'
 
-const userInfo = () => {
-  // 对于已经登录的用户，显示什么？后面再做
+const UserInfo: FC = () => {
+  const nav = useNavigate()
+  const { data } = useRequest(getUserInfoService)
+  const { username, nickname } = data || {}
 
-  return (
+  function loginOut() {
+    removeToken()
+    message.success('退出成功')
+    nav(LOGIN_PATHNAME)
+  }
+
+  const UserInfo = (
+    <>
+      <span style={{ color: '#e8e8e8' }}>
+        <UserOutlined />
+        {nickname}
+      </span>
+      <Button type="link" onClick={loginOut}>
+        退出
+      </Button>
+    </>
+  )
+
+  const Login = (
     <>
       <Link to={LOGIN_PATHNAME}>登录</Link>
     </>
   )
+
+  return <div>{username ? UserInfo : Login}</div>
 }
 
-export default userInfo
+export default UserInfo
