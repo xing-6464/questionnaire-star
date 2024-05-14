54c60c4aca0cac195e7e421e2862e2be58243066
xing-6464 <17621404964@163.com>
2024-05-14 17:54:04 +0800

    chore: 把组件分组显示在组件库中

--- src/pages/question/Edit/ComponentLib.tsx
+++ src/pages/question/Edit/ComponentLib.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Typography } from 'antd'
-import { componentConfGroup } from '../../../components/QuestionComponents'
+import { componentConfGroup, type ComponentConfType } from '../../../components/QuestionComponents'
+import styles from './ComponentLib.module.scss'
 
 const { Title } = Typography
+
+function genComponent(c: ComponentConfType) {
+  const { title, type, Component } = c
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.component}>
+        <Component />
+      </div>
+    </div>
+  )
+}
 
 const ComponentLib: React.FC = () => {
   return (
@@ -15,6 +28,7 @@ const ComponentLib: React.FC = () => {
             <Title level={3} style={{ fontSize: '16px', marginTop: index > 0 ? '20px' : '0' }}>
               {groupName}
             </Title>
+            <div>{components.map(c => genComponent(c))}</div>
           </div>
         )
       })}
