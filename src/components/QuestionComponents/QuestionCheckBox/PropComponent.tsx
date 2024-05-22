82eff4356dc79e47a4d864ccb9361c76058134da
xing-6464 <17621404964@163.com>
2024-05-22 22:17:03 +0800

    chore: 开发QuestionCheckbox属性表单-可动态添加删除选项

--- src/components/QuestionComponents/QuestionCheckBox/PropComponent.tsx
+++ src/components/QuestionComponents/QuestionCheckBox/PropComponent.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { QuestionCheckBoxPropsType } from './interface'
-import { Checkbox, Form, Input } from 'antd'
+import { OptionType, QuestionCheckBoxPropsType, questionCheckBoxDefaultProps } from './interface'
+import { Button, Checkbox, Form, Input, Space } from 'antd'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
+import { nanoid } from '@reduxjs/toolkit'
 
 const PropComponent: React.FC<QuestionCheckBoxPropsType> = props => {
-  const { title, isVertical, list = [], onChange, disabled } = props
+  const {
+    title,
+    isVertical,
+    list = [],
+    onChange,
+    disabled,
+  } = { ...questionCheckBoxDefaultProps, ...props }
   const [form] = Form.useForm()
 
   function handleValuesChange() {
-    const values = form.getFieldsValue()
-    onChange?.(values)
+    const newValues = form.getFieldsValue()
+    const { list } = newValues
+    list.forEach((opt: { value: string }) => {
+      if (opt.value) return
+      opt.value = nanoid(5)
+    })
+    onChange?.(newValues)
   }
 
   return (
@@ -22,7 +35,55 @@ const PropComponent: React.FC<QuestionCheckBoxPropsType> = props => {
       <Form.Item name="title" label="标题" rules={[{ required: true, message: '请输入标题' }]}>
         <Input />
       </Form.Item>
+      <Form.Item name="list" label="选项">
+        <Form.List name="list">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name }, index) => {
+                return (
+                  <Space key={key} align="baseline">
+                    <Form.Item name={[name, 'checked']} valuePropName="checked">
+                      <Checkbox></Checkbox>
+                    </Form.Item>
+                    <Form.Item
+                      name={[name, 'text']}
+                      rules={[
+                        { required: true, message: '请输入选项内容' },
+                        {
+                          validator: (_, text) => {
+                            const { list = [] } = form.getFieldsValue()
 
+                            let num = 0
+                            list.forEach((opt: OptionType) => {
+                              if (opt.text === text) num++ // 记录text相同的个数, 只有一个
+                            })
+                            if (num === 1) return Promise.resolve()
+                            return Promise.reject(new Error('选项内容不能相同'))
+                          },
+                        },
+                      ]}
+                    >
+                      <Input placeholder="请输入选项内容" />
+                    </Form.Item>
+                    {/* 删除按钮 */}
+                    {index > 0 && <MinusCircleOutlined onClick={() => remove(name)} />}
+                  </Space>
+                )
+              })}
+              <Form.Item>
+                <Button
+                  type="link"
+                  block
+                  onClick={() => add({ text: '', value: '', checked: false })}
+                  icon={<PlusOutlined />}
+                >
+                  添加选项
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+      </Form.Item>
       <Form.Item name="isVertical" valuePropName="checked">
         <Checkbox>是否垂直</Checkbox>
       </Form.Item>
