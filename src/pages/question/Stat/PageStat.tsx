3cecd8b5c586a34e8220cb9819616e6f4ba5eec7
xing-6464 <17621404964@163.com>
2024-06-01 18:07:12 +0800

    chore: 显示统计表格-需根据componentList自定义columns

--- src/pages/question/Stat/PageStat.tsx
+++ src/pages/question/Stat/PageStat.tsx
@@ -3,11 +3,15 @@ import type { PropsType } from './types'
 import { useRequest } from 'ahooks'
 import { getQuestionStatListService } from '../../../services/stat'
 import { useParams } from 'react-router-dom'
-import { Spin, Typography } from 'antd'
+import { Spin, Table, Typography } from 'antd'
+import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import Loading from '../../../components/Loading'
 
 const { Title } = Typography
 
 function PageStat(props: PropsType) {
+  const { selectedComponentId, setSelectedComponentId, setSelectedComponentType } = props
   const { id = '' } = useParams()
 
   const [total, setTotal] = useState()
@@ -26,14 +30,38 @@ function PageStat(props: PropsType) {
     }
   )
 
+  const { componentList } = useGetComponentInfo()
+  const columns = componentList.map(c => {
+    const { fe_id, title, type, props = {} } = c
+
+    const colTitle = props.title || title
+
+    return {
+      // title: colTitle,
+      title: (
+        <div
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            setSelectedComponentId(fe_id)
+            setSelectedComponentType(type)
+          }}
+        >
+          <span style={{ color: fe_id === selectedComponentId ? '#1890ff' : 'inherit' }}>
+            {colTitle}
+          </span>
+        </div>
+      ),
+      dataIndex: fe_id,
+    }
+  })
+  const dataSource = list.map((i: any) => ({ ...i, key: i._id }))
+  const TableElement = <Table columns={columns} dataSource={dataSource} pagination={false}></Table>
+
   return (
     <div>
       <Title level={3}>答卷数量：{!loading && total}</Title>
-      {loading && (
-        <div style={{ textAlign: 'center', marginTop: 60 }}>
-          <Spin />
-        </div>
-      )}
+      {loading && <Loading />}
+      {!loading && TableElement}
     </div>
   )
 }
