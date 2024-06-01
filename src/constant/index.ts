eb3f9fd4e50d9c1b98996869edeb158c6e93bad8
xing-6464 <17621404964@163.com>
2024-06-01 18:52:13 +0800

    chore: 显示统计列表-实现分页功能

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,3 +2,4 @@ export const LIST_SEARCH_PARAM_KEY = 'keyword'
 export const LIST_PAGE_PARAM_KEY = 'page'
 export const LIST_PAGE_SIZE_PARAM_KEY = 'pageSize'
 export const LIST_PAGE_SIZE = 10
+export const STAT_PAGE_SIZE = 10 // 统计页面每页显示的条数
--- src/pages/question/Stat/PageStat.tsx
+++ src/pages/question/Stat/PageStat.tsx
@@ -3,10 +3,10 @@ import type { PropsType } from './types'
 import { useRequest } from 'ahooks'
 import { getQuestionStatListService } from '../../../services/stat'
 import { useParams } from 'react-router-dom'
-import { Spin, Table, Typography } from 'antd'
-import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import { Pagination, Table, Typography } from 'antd'
 import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
 import Loading from '../../../components/Loading'
+import { STAT_PAGE_SIZE } from '../../../constant'
 
 const { Title } = Typography
 
@@ -16,12 +16,15 @@ function PageStat(props: PropsType) {
 
   const [total, setTotal] = useState()
   const [list, setList] = useState([])
+  const [page, setPage] = useState(1)
+  const [pageSize, setPageSize] = useState(STAT_PAGE_SIZE)
   const { loading } = useRequest(
     async () => {
-      const res = await getQuestionStatListService(id, { page: 1, pageSize: 10 })
+      const res = await getQuestionStatListService(id, { page, pageSize })
       return res
     },
     {
+      refreshDeps: [id, page, pageSize],
       onSuccess(res) {
         const { total, list = [] } = res
         setTotal(total)
@@ -55,7 +58,23 @@ function PageStat(props: PropsType) {
     }
   })
   const dataSource = list.map((i: any) => ({ ...i, key: i._id }))
-  const TableElement = <Table columns={columns} dataSource={dataSource} pagination={false}></Table>
+  const TableElement = (
+    <>
+      <Table columns={columns} dataSource={dataSource} pagination={false}></Table>
+      <div style={{ textAlign: 'center', marginTop: '18px' }}>
+        <Pagination
+          total={total}
+          pageSize={pageSize}
+          current={page}
+          onChange={page => setPage(page)}
+          onShowSizeChange={(page, pageSize) => {
+            setPage(page)
+            setPageSize(pageSize)
+          }}
+        />
+      </div>
+    </>
+  )
 
   return (
     <div>
