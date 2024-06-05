bf4b25ca77af9ecf1dd474138c5204acb4ea00dd
xing-6464 <17621404964@163.com>
2024-06-05 08:26:04 +0800

    chore: 抽离公共代码antd和react，合理使用HTTP缓存

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,36 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  webpack: {
+    configure(webpackConfig) {
+      if (webpackConfig.mode === 'production') {
+        // 拆了 ant design 和 react-dom 两个包，提高性能
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            antd: {
+              name: 'antd-chunk',
+              test: /antd/,
+              priority: 100,
+            },
+            reactDom: {
+              name: 'react-dom-chunk',
+              test: /react-dom/,
+              priority: 99,
+            },
+            vendors: {
+              name: 'vendors-chunk',
+              test: /node_modules/,
+              priority: 98,
+            },
+          },
+        }
+      }
+      return webpackConfig
+    },
+  },
   devServer: {
     port: 8000,
     proxy: {
