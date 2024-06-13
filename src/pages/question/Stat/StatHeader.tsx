77e4524c57b562564d0548750fe7d073aae35227
xing-6464 <17621404964@163.com>
2024-06-13 09:51:55 +0800

    chore: 项目部署前准备

--- src/pages/question/Stat/StatHeader.tsx
+++ src/pages/question/Stat/StatHeader.tsx
@@ -53,7 +53,7 @@ function StatHeader() {
     if (!isPublished) return null
 
     // generate QR code and link
-    const url = `http://localhost:3000/question/${id}`
+    const url = `http://1.94.45.254:8001/question/${id}`
 
     const QRCodeElem = (
       <div style={{ textAlign: 'center' }}>
